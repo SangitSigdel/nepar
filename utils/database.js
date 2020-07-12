7cd35ce7e8d0abc4cde1c3104aa89481b085a021
Sangit Sigdel <sangit.sigdel@gmail.com>
2020-07-12 18:23:51 +0545

    changed default value of database credentials to cofig.env variables

--- utils/database.js
+++ utils/database.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
 
 const host = process.env.SQL_HOST
 const user = process.env.SQL_USER
 const password = process.env.SQL_PASSWORD
 const database = process.env.SQL_DATABASE
 
+console.log(host,user,password,database)
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'sangit',
-    password: 'schoolpage536383',
-    database: 'nepar'
+    host: `${host}`,
+    user: `${user}`,
+    password: `${password}`,
+    database: `${database}`
 });
 
 module.exports = con
