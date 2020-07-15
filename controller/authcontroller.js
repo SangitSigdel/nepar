d588df539458a9aa475335f24378adbdd93a1eaa
sangit sigdel <sangit.sigdel@gmail.com>
2020-07-14 20:11:08 -0700

    some preetier formating

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,39 +1,42 @@
-const jwt = require('jsonwebtoken')
-const appError = require('../utils/appError')
-const db = require('../utils/database')
+const jwt = require("jsonwebtoken");
+const appError = require("../utils/appError");
+const db = require("../utils/database");
 
 exports.protect = async (req, res, next) => {
+  if (
+    !req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  )
+    return next(new appError("please login to continue", 401));
 
-    if (!req.headers.authorization && req.headers.authorization.startsWith('Bearer')) return next(new appError('please login to continue', 401))
+  const token = req.headers.authorization.split(" ")[1];
 
-    const token = req.headers.authorization.split(' ')[1]
+  // console.log(token)
 
-    // console.log(token)
+  try {
+    // get data from json web token
 
-    try {
+    const decode = await jwt.verify(token, process.env.JWT_SECRET);
 
-        // get data from json web token
+    await db.query(`SELECT * FROM users WHERE id='${decode.id}'`, function (
+      error,
+      results,
+      fields
+    ) {
+      if (results.length == 0)
+        return next(new appError("sorry user not found", 404));
 
-        const decode = await jwt.verify(token, process.env.JWT_SECRET)
+      req.user = results;
 
-        await db.query(`SELECT * FROM users WHERE id='${decode.id}'`, function (error, results, fields) {
+      // console.log(req.user)
+    });
 
-            if (results.length == 0) return next(new appError('sorry user not found', 404))
+    //temp for checking i am sending response to the user.
+    res.send("success");
 
-            req.user = results
-
-            // console.log(req.user)
-        })
-
-        //temp for checking i am sending response to the user.
-        res.send('success')
-
-        //use next for this middleware after security checkups
-        // next()
-    }
-
-    catch (err) {
-        return next(new appError(err, 500))
-    }
-
-}
+    //use next for this middleware after security checkups
+    // next()
+  } catch (err) {
+    return next(new appError(err, 500));
+  }
+};
--- controller/orderController.js
+++ controller/orderController.js
@@ -1,35 +1,31 @@
-const catchAsync = require('../utils/catchAsync')
-const appError = require('../utils/appError')
-const query = require('../controller/mysql/query')
+const catchAsync = require("../utils/catchAsync");
+const appError = require("../utils/appError");
+const query = require("../controller/mysql/query");
 
-exports.createOrder = catchAsync(async (req,res,next)=>{
+exports.createOrder = catchAsync(async (req, res, next) => {
+  req.body.forEach((element) => {
+    element.customer_id = req.params.customer_id;
+  });
 
-    req.body.forEach(element => {
-        element.customer_id = req.params.customer_id
+  let keys = Object.keys(req.body[0]);
 
+  let values = req.body.map((obj) => keys.map((key) => obj[key]));
+
+  console.log(keys, values);
+
+  req.query =
+    "INSERT INTO orders_vendors" + " (" + keys.join(",") + ") VALUES ?";
+
+  await query.query_data(req, next, values).then((fields) => {
+    res.status(200).json({
+      status: "success",
+      message: "order created successfully",
     });
+  });
+});
 
-    let keys = Object.keys(req.body[0]);
-
-    let values = req.body.map(obj => keys.map(key => obj[key]));
-
-    console.log(keys, values)
-
-    req.query = 'INSERT INTO orders_vendors' + ' (' + keys.join(',') + ') VALUES ?'
-
-    await query.query_data(req, next, values).then(fields => {
-      
-        res.status(200).json({
-            status:'success',
-            message: 'order created successfully'
-        })
-
-    })
-})
-
-exports.getCustomerOrderInfo = catchAsync (async (req,res,next)=>{
-
-    req.query = `SELECT orders_vendors.id AS order_id,orders_vendors.date as order_date,product_vendor.name AS product_name,quantity,price AS unit_price, (quantity*price) AS total, nepar_users.name AS vendor_name,contact
+exports.getCustomerOrderInfo = catchAsync(async (req, res, next) => {
+  req.query = `SELECT orders_vendors.id AS order_id,orders_vendors.date as order_date,product_vendor.name AS product_name,quantity,price AS unit_price, (quantity*price) AS total, nepar_users.name AS vendor_name,contact
                     FROM orders_vendors
                     LEFT JOIN product_vendor
                     ON orders_vendors.vendor_product_id = product_vendor.id
@@ -37,18 +33,18 @@ exports.getCustomerOrderInfo = catchAsync (async (req,res,next)=>{
                     ON product_vendor.vendor_id = vendor_info.id
                     LEFT JOIN nepar_users
                     ON vendor_info.nepar_user_id = nepar_users.id
-                    WHERE orders_vendors.customer_id=${req.params.customer_id}`
+                    WHERE orders_vendors.customer_id=${req.params.customer_id}`;
 
-    await query.query_data(req,next).then(fields=>{
-        res.status(200).json({
-            status:'success',
-            data: fields
-        })
-    })
-})
+  await query.query_data(req, next).then((fields) => {
+    res.status(200).json({
+      status: "success",
+      data: fields,
+    });
+  });
+});
 
-exports.getVendorOrderInfo = catchAsync (async (req,res,next)=>{
-    req.query = `SELECT orders_vendors.id as order_id,orders_vendors.date as order_date,product_vendor.name AS product_name, quantity,price as unit_price,nepar_users.name AS customer_name, nepar_users.contact AS customer_contact,status 
+exports.getVendorOrderInfo = catchAsync(async (req, res, next) => {
+  req.query = `SELECT orders_vendors.id as order_id,orders_vendors.date as order_date,product_vendor.name AS product_name, quantity,price as unit_price,nepar_users.name AS customer_name, nepar_users.contact AS customer_contact,status 
                     FROM orders_vendors 
                     LEFT JOIN product_vendor 
                     ON orders_vendors.vendor_product_id= product_vendor.id 
@@ -56,11 +52,11 @@ exports.getVendorOrderInfo = catchAsync (async (req,res,next)=>{
                     ON orders_vendors.customer_id = customer_info.id 
                     LEFT JOIN nepar_users 
                     ON customer_info.nepar_user_id = nepar_users.id 
-                    WHERE product_vendor.vendor_id= ${req.params.vendor_id} ORDER BY orders_vendors.date `
-    await query.query_data(req,next).then(fields=>{
-        res.status(200).json({
-            status:'success',
-            data: fields
-        })
-    })
-})
+                    WHERE product_vendor.vendor_id= ${req.params.vendor_id} ORDER BY orders_vendors.date `;
+  await query.query_data(req, next).then((fields) => {
+    res.status(200).json({
+      status: "success",
+      data: fields,
+    });
+  });
+});
